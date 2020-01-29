c6ed6ff6defd3509e0df02ab79f598a5811bad05
Ian Fabs <ianfabs@outlook.com>
2020-01-29 18:47:42 -0500

    Renamed chars to url

---
+++ url.ts
@@ -0,0 +1,16 @@
+let i: number;
+export default '_-' + String.fromCharCode(
+  // ASCII codes for 0...9
+  i = 48, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1,
+  // ASCII codes for A...Z
+  i += 8, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1,
+  // ASCII codes for a...z
+  i += 7, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1
+)
